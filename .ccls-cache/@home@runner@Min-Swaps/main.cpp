87d19eb1d3973c2fdc7081b365c975abf75d4d47
Aditya Vishwakarrma <79763598+adityavishwakarrma@users.noreply.github.com>
2022-06-19 14:01:35 +0000

    first

--- .ccls-cache/@home@runner@Min-Swaps/main.cpp
+++ .ccls-cache/@home@runner@Min-Swaps/main.cpp
@@ -12,9 +12,9 @@ int countMinSwaps(vector<int> arr){
   sort(arrInd.begin(), arrInd.end());
 
   unordered_map<int, bool> visited;
-  for(auto e : arr){
-    visited[e] = 0;
-  }
+  // for(auto e : arr){
+  //   visited[e] = 0;
+  // }
 
   int i = 0;
   int noOfSwaps = 0;
@@ -28,10 +28,13 @@ int countMinSwaps(vector<int> arr){
     while(!visited[currentInd]){
       noOfElements++;
       visited[currentInd] = 1;
+      cout << arrInd[currentInd].first << " ";
       int toGoIndex = arrInd[currentInd].second;
       currentInd = toGoIndex;
+      
       insideWhileLoop = 1;
-      cout << arrInd[currentInd].first << " ";
+      
+      
     }
 
     if(insideWhileLoop){
@@ -53,51 +56,3 @@ int main() {
   
   return 0;
 }
-
-/*
-int countMinSwaps(vector<int> arr){
-  int n = arr.size();
-  vector<pair<int,int>> arrInd;
-
-  for(int i=0; i<n; i++){
-    arrInd.push_back({arr[i], i});
-  }
-
-  sort(arrInd.begin(), arrInd.end());
-
-  unordered_map<int, bool> visited;
-  for(auto e : arr){
-    visited[e] = 0;
-  }
-
-  int i = 0;
-  int noOfSwaps = 0;
-  while(i<n){
-    
-    int currentInd = i;
-    int noOfElements = 0;
-
-    bool insideWhileLoop = 0;
-    
-    while(!visited[currentInd]){
-      noOfElements++;
-      visited[currentInd] = 1;
-      int toGoIndex = arrInd[currentInd].second;
-      currentInd = toGoIndex;
-      insideWhileLoop = 1;
-      cout << arr[]
-    }
-
-    if(insideWhileLoop){
-      noOfSwaps += noOfElements-1;
-      cout << "No Of Elements " << noOfElements << "\n";
-    }
-     
-    
-    i++;
-  }
-  
-  return noOfSwaps;
-}
-
-*/
